246bb8e96669aa9c8dc9df8eab23d541f08cf38a
AntonyCondori <acondoricai@unsa.edu.pe>
2024-12-02 02:47:57 -0500

    Registrando los modelos en el administrador

--- sistema_reviews/sistema/admin.py
+++ sistema_reviews/sistema/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Usuario, Libro, Review
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Libro)
+admin.site.register(Review)
